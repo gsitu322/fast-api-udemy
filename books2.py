d2376151c36fffc53e39741ffac9fb543e659128
Gordon Situ <gsitu322@gmail.com>
2025-08-29 15:10:12 -0700

    Added more put, delete, and get methods

--- books2.py
+++ books2.py
@@ -51,12 +51,38 @@ books = [
 async def read_all_books():
     return books
 
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in books:
+        if (book.id == book_id):
+            return book
+
+@app.get("/books/")
+async def read_book_by_rating(book_rating: int):
+    books_to_return = []
+    for book in books:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+
+    return books_to_return
 
 @app.post("/create_book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
     books.append(find_book_id(new_book))
 
+@app.put("/books/update_book")
+async def update_book(book: BookRequest):
+    for i in range(len(books)):
+        if books[i].id == book.id:
+            books[i] = book
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for i in range(len(books)):
+        if books[i].id == book_id:
+            books.pop(i)
+            break
 
 def find_book_id(book: Book):
     book.id = 1 if len(books) == 0 else books[-1].id + 1
