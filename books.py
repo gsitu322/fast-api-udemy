88fadaef2292c097093059979eedc48154a9b910
Gordon Situ <gsitu322@gmail.com>
2025-08-29 13:47:48 -0700

    Using lots of get methods

--- books.py
+++ books.py
@@ -2,10 +2,43 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-books = {
-    "title": "Title one", "author": "Author one",
-}
+books = [
+    {"title": "Title one", "author": "Author one", "category": "science"},
+    {"title": "Title two", "author": "Author two", "category": "science"},
+    {"title": "Title three", "author": "Author three", "category": "history"},
+    {"title": "Title four", "author": "Author four", "category": "math"},
+    {"title": "Title five", "author": "Author five", "category": "math"},
+    {"title": "Title six", "author": "Author two", "category": "math"}
+]
 
 @app.get("/books")
-async def first_api():
-    return {"message": "Hello World"}
+async def read_all_books():
+    return books
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in books:
+        if book.get("title").casefold() == book_title.casefold():
+            return book
+
+    return None
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+
+    for book in books:
+        if book.get("category").casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author:str, category: str):
+    books_to_return = []
+
+    for book in books:
+        if book.get("author").casefold() == book_author.casefold() and book.get("category").casefold() == category.casefold() :
+            books_to_return.append(book)
+
+    return books_to_return
