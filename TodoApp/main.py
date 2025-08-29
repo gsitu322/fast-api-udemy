e365820812bb281fd32d942204f707529fdf93e0
Gordon Situ <gsitu322@gmail.com>
2025-08-29 16:30:12 -0700

    Updated main and models

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+from database import SessionLocal
 from database import engine
+
+from typing import Annotated
+from sqlalchemy.orm import Session
 import models
+from models import Todos
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todos).all()
