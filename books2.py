74505f834233b463b6aae39b9c38e678072439ca
Gordon Situ <gsitu322@gmail.com>
2025-08-29 15:22:05 -0700

    Added publish date

--- books2.py
+++ books2.py
@@ -11,13 +11,15 @@ class Book:
     author: str
     description: str
     rating: int
+    publish_date: int
 
-    def __init__(self, id: int, title: str, author: str, description: str, rating: int):
+    def __init__(self, id: int, title: str, author: str, description: str, rating: int, publish_date: int):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.publish_date = publish_date
 
 class BookRequest(BaseModel):
     id: Optional[int] = Field(description="ID is not required on create", default=None)
@@ -25,6 +27,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+    publish_date: int = Field(gt=0, lt=2300)
 
     model_config = {
         "json_schema_extra": {
@@ -32,24 +35,35 @@ class BookRequest(BaseModel):
                 "title": "A new book",
                 "author": "Gordon",
                 "description": "A description of a book",
-                "rating": 5
+                "rating": 5,
+                "publish_date": 2025
             }
         }
     }
 
 
 books = [
-    Book(1, "Computer Science Pro", "Gordon", "Some Description", 5),
-    Book(2, "My Story", "Max", "His story", 3),
-    Book(3, "Be Fast with FastAPI", "Roby", "FastAPI", 4),
-    Book(4, "Be Slow with FastAPI", "Roby", "Slow FastAPI", 1),
-    Book(5, "Python's Best Seller", "Python", "List of python best sellers", 4),
-    Book(6, "Harry Potter 1", "JK Rowling", "Harry Potter", 5),
+    Book(1, "Computer Science Pro", "Gordon", "Some Description", 5, 2021),
+    Book(2, "My Story", "Max", "His story", 3, 2022),
+    Book(3, "Be Fast with FastAPI", "Roby", "FastAPI", 4, 2015),
+    Book(4, "Be Slow with FastAPI", "Roby", "Slow FastAPI", 1, 2015),
+    Book(5, "Python's Best Seller", "Python", "List of python best sellers", 4, 2007),
+    Book(6, "Harry Potter 1", "JK Rowling", "Harry Potter", 5, 2004),
 ]
 
 @app.get("/books")
 async def read_all_books():
     return books
+
+@app.get("/books/bypublushDate/")
+async def read_book_by_publish_date(publish_date: int):
+    books_to_return = []
+
+    for book in books:
+        if book.publish_date == publish_date:
+            books_to_return.append(book)
+
+    return books_to_return
 
 @app.get("/books/{book_id}")
 async def read_book(book_id: int):
