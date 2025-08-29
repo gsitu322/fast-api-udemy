45f1e046adf05dda51a70d29fab81b1144499b13
Gordon Situ <gsitu322@gmail.com>
2025-08-29 14:54:53 -0700

    Added configuration for model

--- books2.py
+++ books2.py
@@ -20,11 +20,22 @@ class Book:
         self.rating = rating
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description="ID is not required on create", default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author": "Gordon",
+                "description": "A description of a book",
+                "rating": 5
+            }
+        }
+    }
 
 
 books = [
