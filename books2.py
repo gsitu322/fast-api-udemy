a190a2c00d6749c73a32c57117692fafe6530862
Gordon Situ <gsitu322@gmail.com>
2025-08-29 14:48:42 -0700

    Creating book with BookRequest model

--- books2.py
+++ books2.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -16,6 +19,14 @@ class Book:
         self.description = description
         self.rating = rating
 
+class BookRequest(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+
+
 books = [
     Book(1, "Computer Science Pro", "Gordon", "Some Description", 5),
     Book(2, "My Story", "Max", "His story", 3),
@@ -28,3 +39,14 @@ books = [
 @app.get("/books")
 async def read_all_books():
     return books
+
+
+@app.post("/create_book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    books.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(books) == 0 else books[-1].id + 1
+    return book
