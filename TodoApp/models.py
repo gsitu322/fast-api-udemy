d84fdc8853753ba930022eaa9f3f6c11630b79f7
Gordon Situ <gsitu322@gmail.com>
2025-09-02 13:07:32 -0700

    Saving user to database

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,6 +1,7 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
+
 class Users(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
