af3977bcd28686dc789216903f9bff630bf0016f
Gordon Situ <gsitu322@gmail.com>
2025-09-02 14:32:21 -0700

    Added role and admin routes

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from database import engine
 import models
 
-from routers import auth, todos
+from routers import admin, auth, todos
 
 app = FastAPI()
 
@@ -10,3 +10,5 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+
+app.include_router(admin.router)
