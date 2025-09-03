622d9f8ba3a42ec14341234ebd0bed25be61efc8
Gordon Situ <gsitu322@gmail.com>
2025-09-03 00:41:54 -0700

    Seetting up database

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,12 +3,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # sqlite setup
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///todosapp.db'
-#engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = 'sqlite:///todosapp.db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 # protgres setup - test password, welps
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Gordon123!@localhost:5432/TodoApplicationDatabase'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Gordon123!@localhost:5432/TodoApplicationDatabase'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # mysql setup
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Gordon123!@localhost:3306/TodoApplicationDatabase'
